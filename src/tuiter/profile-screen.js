4d0008c41fbcf648bc92b68a6dd393a7290c0139
Schromeo <56519950+Schromeo@users.noreply.github.com>
2023-06-20 13:59:20 -0700

    Add files via upload

--- src/tuiter/profile-screen.js
+++ src/tuiter/profile-screen.js
@@ -9,42 +9,48 @@ function ProfileScreen() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const save = () => { dispatch(updateUserThunk(profile)); };
-    useEffect(() => {
-//        const { payload } = await dispatch(profileThunk());
-//        setProfile(payload);
+    useEffect(async () => {
+        const { payload } = await dispatch(profileThunk());
+        setProfile(payload);
     }, []);
 
     return (
     <div>
         <h1>Profile Screen</h1>
-        {profile && (<div>
-        <div>
-        <label>First Name</label>
-        <input type="text" value={profile.firstName}
-        onChange={(event) => {
-        const newProfile = {
-        ...profile, firstName: event.target.value,
-        };
-        setProfile(newProfile);
-        }}/>
-        </div>
-        <div>
-        <label>Last Name</label>
-        <input type="text" value={profile.lastName}
-        onChange={(event) => {
-        const newProfile = {
-        ...profile, lastName: event.target.value,
-        };
-        setProfile(newProfile);
-        }}/>
-        </div></div>
+        {profile && (
+            <div>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" value={profile.firstName}
+                        onChange={(event) => {
+                            const newProfile = {
+                                ...profile, firstName: event.target.value,
+                            };
+                            setProfile(newProfile);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" value={profile.lastName}
+                        onChange={(event) => {
+                            const newProfile = {
+                                ...profile, lastName: event.target.value,
+                            };
+                            setProfile(newProfile);
+                        }}
+                    />
+                </div>
+            </div>
         )}
         <button
-        onClick={() => {
-        dispatch(logoutThunk());
-        navigate("/login");
-        }}> Logout</button>
+            onClick={() => {
+                dispatch(logoutThunk());
+                navigate("/login");
+            }}>
+            Logout
+        </button>
         <button onClick={save}>Save </button>
     </div>);
-    }
+}
 export default ProfileScreen;
